6213916f15bf95155a11b7d9d39505a12812955b
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-06-25 22:57:56 -0300

    First commit with the bugs

---
+++ README.md
@@ -0,0 +1,27 @@
+# Portifolio Project
+
+> This project has the objective of recreate a Portifolio from a template already provided. Fidelity between template and project is the top priority.
+
+You can see the work by accesing the following link: https://guilhermegiachellin.github.io/PortifolioProject/.
+
+Desktop Version
+
+![image](https://user-images.githubusercontent.com/81584449/121713075-d2873d80-cab2-11eb-8207-2737d54c2392.png)
+![image](https://user-images.githubusercontent.com/81584449/121713218-006c8200-cab3-11eb-9d6b-ce0e65c2207a.png)
+
+Mobile Version
+
+![image](https://user-images.githubusercontent.com/81584449/120885158-d3edcd00-c5bd-11eb-831b-0b2e56ca64eb.png)
+
+## Built With
+
+- HTML
+- CSS
+
+## Authors
+
+👤 **Author**
+
+- GitHub: @GuilhermeGiachellin(https://github.com/GuilhermeGiachellin)
+- LinkedIn: Guilherme Giachellin(https://www.linkedin.com/in/guilherme-giachellin-2599771b9/)
+
---
+++ scrip.js
@@ -0,0 +1,90 @@
+const Hamburguer = document.getElementById('hamburguer');
+const Display = document.getElementById('display_menu');
+const Chmbtn = document.getElementById('chm_btn');
+const Dsport = document.getElementById('ds_port');
+const Dsabt = document.getElementById('ds_abt');
+const Dscnt = document.getElementById('ds_cnt');
+const sec = [];
+const emailInput = document.getElementById('email_input');
+const announce = document.getElementById('form_div');
+const emailValImg = document.getElementById('email_val_img');
+
+function formValidate() {
+  const strVal = emailInput.value;
+  if (strVal.toLowerCase() !== strVal) {
+    announce.style = 'display: inline;';
+    return false;
+  }
+  return true;
+}
+if (emailValImg < 0) {
+  formValidate();
+}
+
+emailInput.addEventListener('input', (event) => {
+  event.preventDefault();
+  const strVal2 = emailInput.value;
+  announce.style = '';
+  if (strVal2.toLowerCase() !== strVal2) {
+    emailInput.style = 'background-image: url(./assets/false.png)';
+  } else {
+    emailInput.style = 'background-image: url(./assets/true.png)';
+  }
+});
+
+for (let i = 1; i < 6; i += 1) {
+  sec.push(document.getElementById(`sec_${i}`));
+}
+
+Hamburguer.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  Display.style = 'display: flex; animation-name: display_menu_animation; animation-duration: 0.5s;';
+  for (let i = 0; i < 5; i += 1) {
+    sec[i].style = 'filter: blur(3px); -webkit-filter: blur(3px);';
+  }
+});
+
+Chmbtn.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  setTimeout(() => { Display.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'; }, 0);
+  setTimeout(() => { Display.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i += 1) {
+    sec[i].style = '';
+  }
+});
+
+Dsport.addEventListener('click' (Event) => {
+  Event.preventDefault();
+  setTimeout(() => { Display.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'; }, 0);
+  setTimeout(() => { Display.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i += 1) {
+    sec[i].style = '';
+  }
+});
+
+Dsabt.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  setTimeout(() => { Display.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'; }, 0);
+  setTimeout(() => { Display.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i += 1) {
+    sec[i].style = '';
+  }
+});
+
+Dscnt.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  setTimeout(() => { Display.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'; }, 0);
+  setTimeout(() => { Display.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i += 1) {
+    sec[i].style = '';
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(e.currentTarget.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
